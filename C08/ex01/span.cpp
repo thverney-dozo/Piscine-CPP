183f4efa5d845be93e35cbe42144285f2f951885
Theo VERNEY <thverney@e1r1p10.42.fr>
2021-01-06 15:16:11 +0100

    ye

--- C08/ex01/span.cpp
+++ C08/ex01/span.cpp
@@ -3,10 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   span.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aeoithd <aeoithd@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thverney <thverney@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 16:56:09 by aeoithd           #+#    #+#             */
-/*   Updated: 2020/12/09 16:56:10 by aeoithd          ###   ########.fr       */
+/*   Updated: 2021/01/06 15:15:55 by thverney         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "span.hpp"
+
+Span::Span() : _max(2147483647)
+{}
+
+Span::Span(unsigned n) : _max(n)
+{}
+
+Span::Span(const Span &cpy)
+{
+	this->_max = cpy._max;
+}
+
+Span &Span::operator=(const Span &affect)
+{
+	this->_max = affect._max;
+	return *this;
+}
 
--- C08/ex01/span.hpp
+++ C08/ex01/span.hpp
@@ -3,10 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aeoithd <aeoithd@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thverney <thverney@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 16:56:12 by aeoithd           #+#    #+#             */
-/*   Updated: 2020/12/09 16:56:12 by aeoithd          ###   ########.fr       */
+/*   Updated: 2021/01/06 15:15:49 by thverney         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include <iostream>
+# include <vector>
+# include <algorithm>
+# include <cmath>
+# include <climits>
+# include <functional>
+
+class Span
+{
+	private:
+		unsigned int 		_max;
+		std::vector<int>  	_list;
+
+	public:
+		Span();
+		Span(const Span &cpy);
+		Span(unsigned int n);
+		Span	&operator=(const Span &affect);
+		virtual	~Span();
+		void	addNumber(int n);
+		void	addRange(std::vector<int>::iterator start, std::vector<int>::iterator end);
+		int		shortestSpan(void);
+		int		longestSpan(void);
+};
+
+#endif
