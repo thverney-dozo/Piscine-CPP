9b370551fd8aa6c6acea23f8f409c998c7b46702
thverney-dozo <theo7420@hotmail.fr>
2021-01-15 23:52:37 +0100

    fix

--- C01/ex07/main.cpp
+++ C01/ex07/main.cpp
@@ -17,51 +17,50 @@
 
 int     main(int ac, char **av)
 {
-    std::string         filename;
-    std::string         s1;
-    std::string         s2;
     std::string         buffer;
-    std::string         output;
 
     if (ac != 4)
     {
         std::cout << "Usage ->(./replace filename s1 s2)" << std::endl;
         return(0);
     }
-    filename = av[1];
-    s1 = av[2];
-    s2 = av[3];
-    output = filename + ".replace";
+    std::string filename = av[1];
+    std::string s1 = av[2];
+    std::string s2 = av[3];
+    std::string output_str = filename + ".replace";
     if (s1.empty())
         std::cout << "s1 is empty" << std::endl;
     if (s2.empty())
         std::cout << "s2 " << (s1.empty()?"also ":"") << "is empty" << std::endl;
     if (s1.empty() || s2.empty())
         return (1);
-    std::ifstream   readFile(filename);
-    if (readFile.fail())
+    std::fstream input;
+    std::fstream output;
+    input.open(filename.c_str(), std::fstream::in);
+    if (!input)
     {
         std::cout << "Error opening file" << std::endl;
-        exit(1);
+        return(1);
     }
-    std::ofstream    outFile(output);
-    if (outFile.fail())
+    output.open(output_str.c_str(), std::fstream::out);
+    if (!output)
     {
+	input.close();
         std::cout << "Error creating file" << std::endl;
-        exit(1);
+        return (1);
     }
-    while (std::getline(readFile, buffer))
+    while (std::getline(input, buffer))
     {
         while (buffer.find(s1) != std::string::npos)
         {
             buffer.replace(buffer.find(s1), s1.length(), s2);
         }
-        outFile << buffer;
-        outFile << std::endl;
+        output << buffer;
+        output << std::endl;
     }
     if (buffer == "")
-        outFile << std::endl;
-    readFile.close();
-    outFile.close();
+        output << std::endl;
+    input.close();
+    output.close();
     return (0);
 }
